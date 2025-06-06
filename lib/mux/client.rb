470181ec513279f3b4280a78313411aa3458746e
Vedran Marčetić <vmarcetic@gmail.com>
2025-06-06 18:41:24 +0200

    changes

--- lib/mux/client.rb
+++ lib/mux/client.rb
@@ -10,6 +10,19 @@ module Mux
         request = MuxRuby::CreateAssetRequest.new
         request.input = url
         request.playback_policy = MuxRuby::PlaybackPolicy.build_from_hash(playback_policy)
+
+        allowed_attributes = %i[passthrough mp4_support normalize_audio per_title_encode test
+                        max_resolution_tier encoding_tier master_access inputs
+                        playback_policies advanced_playback_policies video_quality
+                        static_renditions meta]
+
+        # Apply only whitelisted options
+        options.each do |key, value|
+          if allowed_attributes.include?(key)
+            request.public_send("#{key}=", value)
+          end
+        end
+
         response = assets_api.create_asset(request)
         response.data.id
       end
